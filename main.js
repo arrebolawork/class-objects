d1c0ad44a896c02ae5751ff2e96f27b98ff6dddd
David Arrebola <arrebola.work@gmail.com>
2025-04-11 20:14:58 +0200

    ejercicio warrior

--- main.js
+++ main.js
@@ -34,3 +34,53 @@ class Teacher {
     console.log(`El profesor o profesora de ${this.asignature} y nivel ${this.level} se llama : ${this.name}, edad: ${this.age}, genero: ${this.genre}`);
   };
 }
+// Construye las siguientes clases:
+
+// Warrior:
+class Warrior {
+  // constructor(life, power): Establece el valor de las propiedades life y power
+  constructor(life, power) {
+    this.life = life;
+    this.power = power;
+  }
+  // attack: Devuelve el valor de power del guerrero
+  attack = () => {
+    return this.power;
+  };
+  // defend(damage): resta el valor del parámetro recibido damage al valor de la propiedad life. Después, imprime el valor de la vida restante.
+  defende = (damage) => {
+    this.life = this.life - damage;
+    return console.log(this.life);
+  };
+}
+
+// Maya: extiende de la clase Warrior
+class Maya extends Warrior {
+  // constructor: Aquí establecemos los valores para el maya, que no hay que olvidar que es un guerrero.
+  // drinkColaCao: Suma 10 al poder.
+  drinkColaCao = () => {
+    this.power += 10;
+  };
+}
+// Aztec: extiende de la clase Warrior
+class Aztec extends Warrior {
+  // constructor: Aquí establecemos los valores para el azteca, que no hay que olvidar que es un guerrero.
+  // drinkNesquik: Suma 10 a la vida.
+  drinkNesquik = () => {
+    this.life += 10;
+  };
+}
+
+// Realiza la siguiente cadena de intercambio de golpes.
+const newAzteca = new Aztec(20, 20);
+const newMaya = new Maya(20, 20);
+// Azteca bebe nesquik
+newAzteca.drinkNesquik();
+// Maya bebe Cola Cao
+newMaya.drinkColaCao();
+// Maya ataca a azteca. Azteca defiende.
+newMaya.attack();
+newAzteca.defende(5);
+// Azteca ataca a maya. Maya defiende.
+newAzteca.attack();
+newMaya.defende(5);
